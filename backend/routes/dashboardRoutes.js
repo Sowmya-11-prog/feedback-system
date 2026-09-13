1a22125232ce2003ffd52df9550f46962daae99b
Sowmya-11-prog <korapatisowmyasri8@gmail.com>
2026-09-13 08:21:00 +0530

    Delete backend directory

--- backend/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const protect = require("../middleware/authMiddleware");
-const adminOnly = require("../middleware/adminMiddleware");
-const { getStats } = require("../controllers/dashboardController");
-
-router.get("/stats", protect, adminOnly, getStats);
-
-module.exports = router;
